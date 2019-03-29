fd428dcb92c2085210494a51516ae79132ab4300
Lorraine <lorraine.li@rangle.io>
2019-03-28 20:02:49 -0400

    trying to override scroll on landing page

--- src/components/App.js
+++ src/components/App.js
@@ -1,14 +1,12 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Link } from "react-router-dom";
-import Home from "./Home";
 import Landing from "./landing/Landing";
-import About from "./About";
+import About from "./about/About";
 
 const App = () => (
   <Router>
     <div className="container">
       <Route exact path="/" component={Landing} />
-      <Route exact path="/home" component={Home} />
       <Route exact path="/about" component={About} />
     </div>
   </Router>
--- src/components/landing/Landing.js
+++ src/components/landing/Landing.js
@@ -5,11 +5,31 @@ import Title from "../title/Title";
 import GlobeImage from "../globe/GlobeImage";
 
 export default class Landing extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { loaded: false };
+    this.overrideScroll = this.overrideScroll.bind(this);
+  }
+
+  componentDidMount() {
+    window.addEventListener("mousewheel", this.overrideScroll, false);
+  }
+
+  // componentWillUnmount() {
+  //   document.removeEventListener("scroll", this.overrideScroll);
+  // }
+
   handleOnClick() {
     this.props.history.push("/about");
   }
 
   // function that overrides the scroll
+  overrideScroll() {
+    console.log("@overrideScroll");
+    setTimeout(() => {
+      this.setState({ loaded: true });
+    }, 10);
+  }
 
   // componentWillMount(), override the window's scroll listener with function
 
@@ -24,7 +44,12 @@ export default class Landing extends Component {
       <div className="circle">
         <h1>About</h1>
         <p>asldkadslkajs</p>
+    return (
+      <div>
+        {this.state.loaded}
+        <div className={"landing " + this.state.loaded ? "hide" : "show"} />
+        
       </div>
-    ];
+    );
   }
 }
