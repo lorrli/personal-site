c5ebc45d1b70cd67641a5df5d67b8d34e18a6671
Lor <lli9968@gmail.com>
2019-05-18 17:02:00 -0400

    changed routing to home page

--- index.js
+++ index.js
@@ -1,10 +1,16 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import App from "./components/App";
+import App from "./src/components/App";
 import "./styles/main.scss";
+import { BrowserRouter as Router } from "react-router-dom";
 
 const title = "Hi";
 
-ReactDOM.render(<App />, document.getElementById("app"));
+ReactDOM.render(
+  <Router basename={process.env.PUBLIC_URL}>
+    <App />
+  </Router>,
+  document.getElementById("app")
+);
 
 module.hot.accept();
